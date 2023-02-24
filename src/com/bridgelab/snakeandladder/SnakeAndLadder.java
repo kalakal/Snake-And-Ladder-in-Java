2fa2283357d747c10dfe6204523e181ba58fa9cf
kalakal <sayedfaizanqadri@gmail.com>
2023-02-24 14:21:24 +0530

    Snake and Ladder Problem Added

---
+++ src/com/bridgelab/snakeandladder/SnakeAndLadder.java
@@ -0,0 +1,11 @@
+package com.bridgelab.snakeandladder;
+
+public class SnakeAndLadder {
+
+        static int player=0;
+        public static void main(String[] args) {
+            System.out.println("Well come to Snake and Ladder Game");
+            System.out.println(" Game start player at position  "+player);
+        }
+}
+
