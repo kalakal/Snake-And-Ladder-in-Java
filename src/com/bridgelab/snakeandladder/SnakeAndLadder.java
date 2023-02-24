c12fb57ffb7e64c611a595f7788ddee1d6794ea8
kalakal <sayedfaizanqadri@gmail.com>
2023-02-24 15:42:29 +0530

    Faizan Added option checking part

--- src/com/bridgelab/snakeandladder/SnakeAndLadder.java
+++ src/com/bridgelab/snakeandladder/SnakeAndLadder.java
@@ -1,11 +1,24 @@
 package com.bridgelab.snakeandladder;
-// UC-2 player rolls the die to get number between 1 to 6.
+// UC-3 Player checks for option no play snake or ladder.
 public class SnakeAndLadder {
-    static int player=0;
+    static int player = 0;
+
     public static void main(String[] args) {
         System.out.println("Well come to Snake and Ladder Game");
-        System.out.println(" Game start player at position  "+player);
-        int dice=(int) (Math.random()*(6)+1);
-        System.out.println("dice : "+dice);
+        System.out.println(" Game start player at position  " + player);
+        int option = (int) (Math.random() * (3) + 1);
+        if (option == 1) {
+            System.out.println("No play");
+        }
+        else if (option == 2) {
+            int dice = (int) (Math.random() * (6) + 1);
+            System.out.println(" player is playing dice : " + dice);
+        }
+        else {
+            int dice = (int) (Math.random() * (6) + 1);
+            System.out.println(" player is playing dice : " + dice);
+        }
+
     }
+
 }
