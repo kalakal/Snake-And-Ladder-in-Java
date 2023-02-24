04aaf565920172b3b103e70fc29cf3a438f032e6
kalakal <sayedfaizanqadri@gmail.com>
2023-02-24 15:10:52 +0530

    Faian added random number between 1 And 6

--- src/com/bridgelab/snakeandladder/SnakeAndLadder.java
+++ src/com/bridgelab/snakeandladder/SnakeAndLadder.java
@@ -1,11 +1,11 @@
 package com.bridgelab.snakeandladder;
-
+// UC-2 player rolls the die to get number between 1 to 6.
 public class SnakeAndLadder {
-
-        static int player=0;
-        public static void main(String[] args) {
-            System.out.println("Well come to Snake and Ladder Game");
-            System.out.println(" Game start player at position  "+player);
-        }
+    static int player=0;
+    public static void main(String[] args) {
+        System.out.println("Well come to Snake and Ladder Game");
+        System.out.println(" Game start player at position  "+player);
+        int dice=(int) (Math.random()*(6)+1);
+        System.out.println("dice : "+dice);
+    }
 }
-
